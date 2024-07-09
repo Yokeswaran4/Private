ce07165b84ed2b607ca9d4acb9eb9a44b1c6f8c0
Yokeswaran4 <160291763+Yokeswaran4@users.noreply.github.com>
2024-07-09 21:37:57 +0530

    done

--- tictactoe.py
+++ tictactoe.py
@@ -1,2 +1,18 @@
 class game:
-  game_no=0
+    game_no=0
+    def __init__(self):
+        game.game_no+=1
+        self.p1='X'
+        self.p2='O'
+        self.char='E'
+        self.arr=[[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
+        print("Welcome to Tic Tac Toe" )
+        print("\t\t\tGame-",game.game_no)
+    def disp(self):
+        for r in self.arr:
+            for m in r:
+                print(m,end='|')
+            print("--------")
+        return
+    def fill1(self):
+        p
