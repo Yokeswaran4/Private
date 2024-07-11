2d56cf6fd6b187f213ab7690257398931b86bb1b
Yokeswaran4 <160291763+Yokeswaran4@users.noreply.github.com>
2024-07-11 13:59:55 +0000

    kkk

--- tictactoe.py
+++ tictactoe.py
@@ -20,8 +20,18 @@ class game:
         while self.arr[i][j]==self.p1 or self.arr[i][j]==self.p2 :
             print('Sorry.Enter the correct row and column:')
             i,j=int(input()),int(input())
-        self.arr[i][j]=self.p1
+        self.arr[i-1][j-1]=self.p1
         return
     def fill2(self):
         print('Player-2:Enter the row and column:')
-        i,j=int(input())
+        i,j=int(input()),int(input())
+        while self.arr[i][j]==self.p1 or self.arr[i][j]==self.p2 :
+            print('Sorry.Enter the correct row and column:')
+            i,j=int(input()),int(input())
+        self.arr[i-1][j-1]=self.p2
+        return
+    def condchk_res(self) :
+        m=0
+        while m<=3 :
+            if self.arr[m][0]==self.arr[m][1] and self.arr[m][1]==self.arr[m][2] :
+                
